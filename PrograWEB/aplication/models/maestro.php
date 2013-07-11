5ab58d5ed222d778aa8092f82346033df60f1ca6
Alma <aurys_dora@hotmail.com>
2013-07-10 20:27:09 -0500

    modelos de alma

--- PrograWEB/aplication/models/maestro.php
+++ PrograWEB/aplication/models/maestro.php
@@ -5,26 +5,26 @@
 	private $rfc;
 	private $id_persona;
 	
-	public get_id_maestro(){
+	public function get_id_maestro(){
                     return this->$id_maestro;
 	}
 	
-	public get_rfc(){
+	public function get_rfc(){
                       return this->$rfc;
 	}
-	public get_id_persona(){
+	public function get_id_persona(){
                       return this->$id_persona;
 	}
 	
 	
-	public set_id_maestro($valor){
+	public function set_id_maestro($valor){
                      $this->$id_maestro=$valor;
 	}
 	
-	public set_rfc($valor){
+	public function set_rfc($valor){
                        $this->$rfc=$valor;
 	}
-	public set_id_persona($valor){
+	public function set_id_persona($valor){
                        $this->$id_persona=$valor;
 	}
 ?>
