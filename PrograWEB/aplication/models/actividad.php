93458adf3f38f9eb581740bf3eefcd7dd4f05876
ManeRs11 <mane-rs11@hotmail.com>
2013-07-10 22:09:44 -0500

    Nuevo modelo

---
+++ PrograWEB/aplication/models/actividad.php
@@ -0,0 +1,109 @@
+/**************************************************************
+                      Modelado Actividad
+**************************************************************/
+
+
+<?php
+
+	// *Variables Globales Privadas
+	private	$id_actividad;
+	private $nombre;
+	private $capacidad;
+	private $lugar;
+	private $precio;
+	private $fecha_ini;
+	private $fecha_fin;
+	private $horario;
+	private $descripcion;
+	private $evento;
+	//----------------------------------------
+
+
+
+	// *Obtener Datos
+	function get_id_actividad(){
+		return $this->$id_actividad;
+	}
+
+	function get_nombre(){
+                return $this->$nombre;
+        }
+
+	function get_capacidad(){
+		return $this->$capacidad;
+	}
+
+	function get_lugar(){
+		return $this->$lugar;
+	}
+
+	function get_precio(){
+		return $this->$precio;
+	}
+
+	function get_fecha_ini(){
+		return $this->$fecha_ini;
+	}
+
+	function get_fecha_fin(){
+		return $this->$fecha_fin;
+	}
+
+	function get_horario(){
+		return $this->$horario;
+	}
+
+	function get_descripcion(){
+		return $this->$descripcion;
+	}
+
+	funcion get_evento(){
+		return $this->$evento;
+	}
+	//----------------------------------------------------------
+
+
+
+	// *Asignar Datos
+	function set_id_actividad( $valor ){
+		$this->$id_activdad = $valor;
+	}
+
+	function set_nombre( $valor ){
+		$this->$nombre = $valor;
+	}
+
+	function set_capacidad( $valor ){
+		$this->$capacidad = $valor;
+	}
+
+	function set_lugar( $valor ){
+		$this->$lugar = $valor;
+	}
+
+	function set_precio( $valor ){
+		$this->$precio = $valor;
+	}
+
+	function set_fecha_ini( $valor ){
+		$this->$fecha_ini = $valor;
+	}
+
+	function set_fecha_fin( $valor ){
+		$this->$fecha_fin = $valor;
+	}
+
+	function set_horario( $valor ){
+		$this->$horario = $valor;
+	}
+
+	function set_descripcion( $valor ){
+		$this->$descripcion = $valor;
+	}
+
+	function set_evento( $valor ){
+		$this->$evento = $evento;
+	}
+	//----------------------------------------------------------
+
+?>
--- PrograWEB/aplication/models/alumno.php~
+++ PrograWEB/aplication/models/alumno.php~
@@ -20,15 +20,15 @@
 
 	// *Obtener Valores
 	function get_id_alumno(){
-		return this->$id_alumno;
+		return $this->$id_alumno;
 	}
 
 	function get_id_persona(){
-		return this->$id_persona;
+		return $this->$id_persona;
 	}
 
 	function get_NoCtrl(){
-		return this->$No_Ctrl;
+		return $this->$No_Ctrl;
 	}
 	//----------------------------------------------
 
@@ -36,15 +36,15 @@
 
 	// *Asignar Valores
 	function set_id_alumno( $valor ){
-                this->$id_alumno = $valor;
+                $this->$id_alumno = $valor;
         }
 
         function set_id_persona( $valor ){
-                this->$id_persona = $valor;
+                $this->$id_persona = $valor;
         }
 
 	function set_NoCtrl( $valor ){
-		this->No_Ctrl = $valor;
+		$this->No_Ctrl = $valor;
 	}
 	//----------------------------------------------
 
