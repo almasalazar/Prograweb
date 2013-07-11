fb02c527a702560769c6888500353783c0faeaac
Ektoor <ektoor007@gmail.com>
2013-07-10 21:01:34 -0500

    actualizacion presentador, presentador_actividad

--- PrograWEB/aplication/models/presentador.php
+++ PrograWEB/aplication/models/presentador.php
@@ -9,27 +9,19 @@
 	private $id_presentador;
 	private $rfc_clave;
 	private $id_persona;
-?>
-//------------------------------------------------------
 
-
-
-
-
-// -> *Modelado
-<?php
-
-
+		// -> *Modelado
+		
 	// *Obtener Valores
-	function get_id_presentador(){
+	public function get_id_presentador(){
 		return this->$id_presentador;
 	}
 
-	function get_rfc_clave(){
+	public function get_rfc_clave(){
 		return this->$rfc_clave;
 	}
 
-	function get_id_persona(){
+	public function get_id_persona(){
 		return this->$id_persona;
 	}
 	//----------------------------------------------
@@ -37,20 +29,18 @@
 
 
 	// *Asignar Valores
-	function set_id_presentador( $valor ){
+	public function set_id_presentador( $valor ){
                 this->$id_presentador = $valor;
         }
 
-        function set_rfc_clave( $valor ){
+    public function set_rfc_clave( $valor ){
                 this->$rfc_clave = $valor;
         }
 
-	function set_id_persona( $valor ){
+	public function set_id_persona( $valor ){
 		this->id_persona = $valor;
 	}
-	//----------------------------------------------
-
-
 ?>
+
 
 
--- PrograWEB/aplication/models/presentador_actividad.php
+++ PrograWEB/aplication/models/presentador_actividad.php
@@ -8,6 +8,31 @@
 <?php
 	private $id_actividad;
 	private $id_presentador;
+	
+// -> *Modelado
+	
+		// *Obtener Valores
+	public function get_id_actividad(){
+		return this->$id_actividad;
+	}
+
+	public function get_id_presentador(){
+		return this->$id_presentador;
+	}
+
+//----------------------------------------------
+
+
+	// *Asignar Valores
+	public function set_id_actividad( $valor ){
+                this->$id_actividad = $valor;
+        }
+
+    public function set_id_presentador( $valor ){
+                this->$id_presentador = $valor;
+        }
+
+//----------------------------------------------
 ?>
 //------------------------------------------------------
 
@@ -15,36 +40,7 @@
 
 
 
-// -> *Modelado
-<?php
 
 
-	// *Obtener Valores
-	function get_id_actividad(){
-		return this->$id_actividad;
-	}
-
-	function get_id_presentador(){
-		return this->$id_presentador;
-	}
-
-	
-	//----------------------------------------------
-
-
-
-	// *Asignar Valores
-	function set_id_actividad( $valor ){
-                this->$id_actividad = $valor;
-        }
-
-        function set_id_presentador( $valor ){
-                this->$id_presentador = $valor;
-        }
-
-	//----------------------------------------------
-
-
-?>
 
 
